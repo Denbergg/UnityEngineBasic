2081669d8d96446021adfad99ef9915f4d36bda4
Denbergg <pwrpower3628@gmail.com>
2023-02-06 21:54:42 +0900

    RollADice - DiceManager, DicePanelUI, Tiles
    1. DiceManager
    - Created to managing dice numbers & rolling dice.
    
    2. DicePanelUI
    - Gets notifications of DiceManager's dice number changes.
    
    3. Tiles
    - Unit of map tile.
    - Created Base class & some derives.

---
+++ RollADice/Assets/02. Scripts/Player.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+        public void Move(Vector3 target)
+        {
+            transform.Translate(target, Space.World);
+        }
+}
+
